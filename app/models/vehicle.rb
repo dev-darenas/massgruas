d3c181a53244df2c32784d4e25712d7cdd89afdb
Duvan Hernandez <duvanherfi@gmail.com>
2020-06-23 09:45:36 -0500

    Styles are added on the Vehicle Screen

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,6 +1,15 @@
 class Vehicle < ApplicationRecord
   belongs_to :enterprise
+
+  validates :placa, presence: true
+  validates :licencia, presence: true
+  validates :clase, presence: true
+  validates :marca, presence: true
+  validates :tipo, presence: true
+  validates :linea, presence: true
+
   has_many :pictures, as: :imageable, dependent: :destroy
+
   accepts_nested_attributes_for :pictures,
                                 reject_if: lambda {
                                     |a| a[:image].blank?
