f3ef1feb60980fd82e1a0cab066b883a610d63f5
Daniel Arenas <darenas@softdremas.co>
2020-07-08 21:17:17 -0500

    Fix javascript

--- app/javascript/packs/transaction.js
+++ app/javascript/packs/transaction.js
@@ -1,25 +1,22 @@
 $(document).on('turbolinks:load', function () {
-    function initializeAutocomplete(id) {
-        var element = document.getElementById(id);
-        if (element) {
-            var autocomplete = new google.maps.places.Autocomplete(element, {
-                types: ['address'],
-                componentRestrictions: {country: 'co'}
-            });
-            google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-        }
+  function initializeAutocomplete(id) {
+    var element = document.getElementById(id);
+    if (element) {
+      var autocomplete = new google.maps.places.Autocomplete(element, {
+        types: ['address'],
+        componentRestrictions: {country: 'co'}
+      });
+      google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
     }
+  }
 
-    function onPlaceChanged() {
-        var place = this.getPlace();
+  function onPlaceChanged() {
+    var place = this.getPlace();
 
-        console.log(place);  // Uncomment this line to view the full object returned by Google API.
+    console.log(place);  // Uncomment this line to view the full object returned by Google API.
+  }
 
-    }
-
-    google.maps.event.addDomListener(window, 'load', function () {
-        initializeAutocomplete('autocomplete_address');
-    });
+  google.maps.event.addDomListener(window, 'load', function () {
+    initializeAutocomplete('autocomplete_address');
+  });
 });
-
-
