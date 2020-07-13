68a138217cbf7bcf7e951d67e81aca63b0758561
Duvan Hernandez <duvanherfi@gmail.com>
2020-07-13 08:52:25 -0500

    Finished calculation of distance between origin and destination

--- app/controllers/api/v1/calculate_distance_controller.rb
+++ app/controllers/api/v1/calculate_distance_controller.rb
@@ -4,11 +4,12 @@ module Api
       def distance
         lat = (params[:lat]).to_f
         lng = (params[:lng]).to_f
-        latb = (params[:latb]).to_f
-        lngb = (params[:lngb]).to_f
+        latb = (params[:lat2]).to_f
+        lngb = (params[:lng2]).to_f
         km = Geocoder::Calculations.distance_between([lat, lng], [latb, lngb])
+        km = km.round(2)
 
-        render json: {distance_km: km}
+        render json: {lat: lat, lng: lng, lat2: latb, lng2: lngb, distance_km: km}
       end
     end
   end
--- app/javascript/packs/transaction.js
+++ app/javascript/packs/transaction.js
@@ -1,69 +1,75 @@
 $(document).on('turbolinks:load', function () {
-  function initializeAutocomplete(id) {
-    var element = document.getElementById(id);
-    if (element) {
-      var autocomplete = new google.maps.places.Autocomplete(element, {
-        types: ['address'],
-        componentRestrictions: {country: 'co'}
-      });
-      if (id === "autocomplete_address"){
-        google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-      }
-      else {
-        google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged2);
-      }
+    function initializeAutocomplete(id) {
+        var element = document.getElementById(id);
+        if (element) {
+            var autocomplete = new google.maps.places.Autocomplete(element, {
+                types: ['address'],
+                componentRestrictions: {country: 'co'}
+            });
+            if (id === "autocomplete_address") {
+                google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+            } else {
+                google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged2);
+            }
+        }
     }
-  }
 
-  function onPlaceChanged() {
-    var place = this.getPlace();
+    function onPlaceChanged() {
+        var place = this.getPlace();
 
-    console.log(place.geometry.location);  // geometry.location Uncomment this line to view the full object returned by Google API.
-    var lat = document.getElementById("lat");
-    var lng = document.getElementById("lng");
-    var lat2 = document.getElementById("lat2");
-    var lng2 = document.getElementById("lng2");
-    var total_km = document.getElementById("total_km")
+        console.log(place.geometry.location);  // geometry.location Uncomment this line to view the full object returned by Google API.
+        var lat = document.getElementById("lat");
+        var lng = document.getElementById("lng");
+        var lat2 = document.getElementById("lat2");
+        var lng2 = document.getElementById("lng2");
+        var total_km = document.getElementById("total_km")
 
-    lat.value = place.geometry.location.lat();
-    lng.value = place.geometry.location.lng();
+        lat.value = place.geometry.location.lat();
+        lng.value = place.geometry.location.lng();
 
-    $.getJSON('/api/v1/calulate_distance', function(resp) {
-      $.each(resp, function(k, v) {
-        console.log(k + ' : ' + v);
-      });
+        $.getJSON('/api/v1/calulate_distance',
+            {
+                lat: lat.value,
+                lng: lng.value,
+                lat2: lat2.value,
+                lng2: lng2.value
+            }, function (resp) {
+                console.log(resp.distance_km);
+            });
+
+    }
+
+    function onPlaceChanged2() {
+        var place = this.getPlace();
+
+        console.log(place.geometry.location);  // Uncomment this line to view the full object returned by Google API.
+
+        var lat = document.getElementById("lat");
+        var lng = document.getElementById("lng");
+        var lat2 = document.getElementById("lat2");
+        var lng2 = document.getElementById("lng2");
+        var total_km = document.getElementById("total_km")
+
+        lat2.value = place.geometry.location.lat();
+        lng2.value = place.geometry.location.lng();
+
+
+        $.getJSON('/api/v1/calulate_distance', {
+            lat: lat.value,
+            lng: lng.value,
+            lat2: lat2.value,
+            lng2: lng2.value
+        }, function (resp) {
+            total_km.value  = resp.distance_km
+        });
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', function () {
+        initializeAutocomplete('autocomplete_address');
     });
 
-  }
-
-  function onPlaceChanged2() {
-    var place = this.getPlace();
-
-    console.log(place.geometry.location);  // Uncomment this line to view the full object returned by Google API.
-
-    var lat = document.getElementById("lat");
-    var lng = document.getElementById("lng");
-    var lat2 = document.getElementById("lat2");
-    var lng2 = document.getElementById("lng2");
-    var total_km = document.getElementById("total_km")
-
-    lat2.value = place.geometry.location.lat();
-    lng2.value = place.geometry.location.lng();
-
-
-    $.getJSON('/api/v1/calulate_distance', function(resp) {
-      $.each(resp, function(k, v) {
-        console.log(k + ' : ' + v);
-      });
+    google.maps.event.addDomListener(window, 'load', function () {
+        initializeAutocomplete('autocomplete_address2');
     });
-
-  }
-
-  google.maps.event.addDomListener(window, 'load', function () {
-    initializeAutocomplete('autocomplete_address');
-  });
-
-  google.maps.event.addDomListener(window, 'load', function () {
-    initializeAutocomplete('autocomplete_address2');
-  });
 });
