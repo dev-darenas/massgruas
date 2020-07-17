b5ea2303c45e2af2fb3c8988fdd86c079a96f351
Daniel Arenas <darenas@softdremas.co>
2020-07-16 21:32:43 -0500

    Added notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,23 @@ module ApplicationHelper
     base64 = Base64.encode64(asset.to_s).gsub(/\s+/, "")
     "data:#{content_type};base64,#{Rack::Utils.escape(base64)}"
   end
+
+  $TYPE_NOTIFICATION = {
+    "notice": "success",
+    "error": "danger"
+  }
+
+  def type_notification(key)
+    $TYPE_NOTIFICATION[key.to_sym]
+  end
+
+  def alert_error(model)
+    if model.errors.any?
+      html_list = ''
+      model.errors.full_messages.each do |message|
+        html_list += content_tag(:li, message)
+      end
+      content_tag(:div, content_tag(:ul, html_list.html_safe), class: "alert alert-dismissible fade show alert-#{type_notification("error")}")
+    end
+  end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,6 @@
 class Account < ApplicationRecord
   belongs_to :enterprise
   has_and_belongs_to_many :clients
+
+  validates :name, presence: true
 end
