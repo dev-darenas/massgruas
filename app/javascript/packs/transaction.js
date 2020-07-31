7f9d31e91f16ad18fd2af44e741d6780c725c5c2
Duvan Hernandez <duvanherfi@gmail.com>
2020-07-31 13:40:30 -0500

    Transaction fixes

--- app/javascript/packs/transaction.js
+++ app/javascript/packs/transaction.js
@@ -1,37 +1,3 @@
-function calcularHoras() {
-    var fecha_y_hora = $("#fecha_Y_hora")
-    var hora_inicio = fecha_y_hora.val().split("T")[1]
-    var hora_final = $("#hora_llegada").val();
-
-    // Expresión regular para comprobar formato
-    var formatohora = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
-
-    // Si algún valor no tiene formato correcto sale
-    if (!(hora_inicio.match(formatohora)
-        && hora_final.match(formatohora))) {
-        return;
-    }
-
-    // Calcula los minutos de cada hora
-    var minutos_inicio = hora_inicio.split(':')
-        .reduce((p, c) => parseInt(p) * 60 + parseInt(c));
-    var minutos_final = hora_final.split(':')
-        .reduce((p, c) => parseInt(p) * 60 + parseInt(c));
-
-    // Si la hora final es anterior a la hora inicial sale
-    if (minutos_final < minutos_inicio) return;
-
-    // Diferencia de minutos
-    var diferencia = minutos_final - minutos_inicio;
-
-    // Cálculo de horas y minutos de la diferencia
-    var horas = Math.floor(diferencia / 60);
-    var minutos = diferencia % 60;
-
-    $('#response_time').val(horas + ':'
-        + (minutos < 10 ? '0' : '') + minutos);
-}
-
 function sumTotalService() {
     var $total_normal_zone = $("#total_normal_zone");
     var $total_red_zone = $("#total_red_zone");
@@ -75,15 +41,6 @@ function sumTotalGastos() {
     var gastos = resultado;
     $ganancias.val(parseFloat(priced) - parseFloat(gastos));
 }
-
-$(document).on('turbolinks:load', function () {
-    $("#hora_llegada").change(function () {
-        calcularHoras();
-    });
-    $("#fecha_Y_hora").change(function () {
-        calcularHoras();
-    });
-});
 
 $(document).on('turbolinks:load', function () {
     function initializeAutocomplete(id) {
--- app/javascript/src/datepicker.js
+++ app/javascript/src/datepicker.js
@@ -1,4 +1,39 @@
+function calcularHoras() {
+    var fecha_y_hora = $("#fecha_Y_hora")
+    var hora_inicio = fecha_y_hora.val().split(" ")[1]
+    var hora_final = $("#hora_llegada").val();
+
+    // Expresión regular para comprobar formato
+    var formatohora = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+
+    // Si algún valor no tiene formato correcto sale
+    if (!(hora_inicio.match(formatohora)
+        && hora_final.match(formatohora))) {
+        return;
+    }
+
+    // Calcula los minutos de cada hora
+    var minutos_inicio = hora_inicio.split(':')
+        .reduce((p, c) => parseInt(p) * 60 + parseInt(c));
+    var minutos_final = hora_final.split(':')
+        .reduce((p, c) => parseInt(p) * 60 + parseInt(c));
+
+    // Si la hora final es anterior a la hora inicial sale
+    if (minutos_final < minutos_inicio) return;
+
+    // Diferencia de minutos
+    var diferencia = minutos_final - minutos_inicio;
+
+    // Cálculo de horas y minutos de la diferencia
+    var horas = Math.floor(diferencia / 60);
+    var minutos = diferencia % 60;
+
+    $('#response_time').val(horas + ':'
+        + (minutos < 10 ? '0' : '') + minutos);
+}
+
 const init2 = () => {
+
     console.log(' init datetimepicker ');
     $('#datetimepicker2').datetimepicker({
         format: 'DD-MM-YYYY HH:mm zz',
@@ -8,9 +43,17 @@ const init2 = () => {
         }
     });
 
+    $("#datetimepicker2").on("change.datetimepicker", function (e) {
+        calcularHoras();
+    });
+
     $('#datetimepicker3').datetimepicker({
         format: 'HH:mm',
         ignoreReadonly: true
+    });
+
+    $("#datetimepicker3").on("change.datetimepicker", function (e) {
+        calcularHoras();
     });
 
     $('#datetimepicker4').datetimepicker({
