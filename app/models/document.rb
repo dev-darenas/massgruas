4178bddd3400b436a5312fcb268e8ada962c0259
Duvan Hernandez <duvanherfi@gmail.com>
2020-08-04 16:52:48 -0500

    Unique document types are added for customers and vehicles

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,6 @@ class Document < ApplicationRecord
   belongs_to :document_type
   belongs_to :documentable, polymorphic: true
   has_many :pictures, as: :imageable
+  validates :document_type_id, uniqueness: { scope: :documentable_type }
+
 end
