1f288874db837f04935c6abc7ef8e81afc7cbcab
Duvan Hernandez <duvanherfi@gmail.com>
2020-08-06 13:57:16 -0500

    service verification and document type that are unique are added and language is verified

--- app/models/document_type.rb
+++ app/models/document_type.rb
@@ -1,3 +1,5 @@
 class DocumentType < ApplicationRecord
   belongs_to :enterprise
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,6 @@ class Service < ApplicationRecord
   belongs_to :enterprise
   has_one :list_price
   has_many :transactions
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
