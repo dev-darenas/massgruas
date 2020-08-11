af7147fcbff64ef4f5ff0c5c846229a86d486985
Duvan Hernandez <duvanherfi@gmail.com>
2020-08-11 15:01:57 -0500

    technician report is finished

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,6 +5,16 @@ class ReportsController < EnterpriseController
   def technical_clearance
     #render plain: @enterprise.technicals.find(params[:technical_id]).inspect
     @technical = @enterprise.technicals.find(params[:technical_id])
+    @total=0.0
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "Liquidación Técnico",
+               template: "reports/technical_clearance.html.slim",
+               title: 'Liquidación Técnico',
+               layout: 'pdf'
+      end
+    end
   end
 
 end
