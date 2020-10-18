c6de9f7f5ee07f4ea0cb51f6fffb3ef6b8a895d7
Daniel Arenas <darenas@softdremas.co>
2020-10-17 19:40:18 -0500

    Fixed config docker

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ masgruas = Enterprise.create(name: 'masgruas')
 masgruas.users.create(email: 'jhernan@masgruas.com', password: '123456789')
 
 softdreams = Enterprise.create(name: 'softdreams')
-masgruas.users.create(email: 'darenas@softdreams.com', password: '123456789')
+softdreams.users.create(email: 'darenas@softdreams.com', password: '123456789')
